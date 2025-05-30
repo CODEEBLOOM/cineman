5ccef33d56b8bb42749910965428cc0dba792135
SonDQ(STLang) <sondoquang3@gmail.com>
2025-05-30 16:14:27 +0700

    First commit and prepare project base for Cinema Management System

---
+++ src/test/java/com/codebloom/cineman/CinemanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebloom.cineman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
