85a9962fbecdb5ea0fb0d3d3c1558dcc117f19b5
SonDQ(STLang) <sondoquang3@gmail.com>
2025-06-19 05:48:24 +0700

    Change name file global exception

--- src/main/java/com/codebloom/cineman/Exception/GlobalException.java
+++ src/main/java/com/codebloom/cineman/Exception/GlobalException.java
@@ -20,7 +20,7 @@ import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalException {
 
     private ErrorResponse errorResponse;
 
